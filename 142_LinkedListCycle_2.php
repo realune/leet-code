62f03fe3aeea4f6b2cf8fdadc8a4965f1aa57dbd
realune <peiu.1220@gmail.com>
2022-07-03 23:15:37 +0100

    142_LinkedListCycle_2

---
+++ 142_LinkedListCycle_2.php
@@ -0,0 +1,40 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function detectCycle($head) {
+        if (is_null($head)) return null;
+        
+        $fast = $head;
+        $slow = $head;
+        
+        while (!is_null($fast)) {
+            if (is_null($fast->next)) return null;
+
+            $fast = $fast->next->next;
+            $slow = $slow->next;
+            
+            if ($fast === $slow) break;
+        }
+        
+        if (is_null($fast)) return null;
+        
+        $slow = $head;
+        while ($slow !== $fast) {
+            $slow = $slow->next;
+            $fast = $fast->next;
+        }
+        return $slow;
+    }
+}
+
