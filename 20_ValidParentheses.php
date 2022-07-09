a635ba7d16885182bc2981708a08dd2d963d8808
realune <peiu.1220.qi@gmail.com>
2022-07-09 23:24:50 +0100

    20_ValidParentheses.php

---
+++ 20_ValidParentheses.php
@@ -0,0 +1,26 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function isValid($s) {
+        $stack = new SplStack();
+
+        foreach (str_split($s) as $c) {
+            if ($c == '(' || $c == '[' || $c == '{') {
+                $stack->push($c);
+            } elseif ($c == ')' && !$stack->isEmpty() && $stack->top() == '(') {
+                $stack->pop();
+            } elseif ($c == ']' && !$stack->isEmpty() && $stack->top() == '[') {
+                $stack->pop();
+            } elseif ($c == '}' && !$stack->isEmpty() && $stack->top() == '{') {
+                $stack->pop();
+            } else {
+                return false;
+            }
+        }
+        return $stack->isEmpty();              
+    }
+}
+
